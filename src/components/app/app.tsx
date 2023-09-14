f7f2b9ef11983e026e03f09e8c4a33113daeab39
Yuniya Kim <yuniyakim@gmail.com>
2023-09-15 00:41:40 +0700

    Create app component

---
+++ src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+}
+
+function App(props: AppProps): JSX.Element {
+  return (
+    <MainPage />
+  );
+}
+
+export default App;
