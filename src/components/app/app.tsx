8de5bd429d8e1177db8278bf2f9df89f3c5f3458
Yuniya Kim <yuniyakim@gmail.com>
2023-09-27 01:54:23 +0700

    Reorder imports

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,17 +1,17 @@
-import { MainPage } from '../../pages/main-page/main-page';
 import {
   BrowserRouter,
   Routes,
   Route
 } from 'react-router-dom';
 import { RoutePath, AuthorizationStatus } from '../../types/enums';
+import { PrivateRoute } from '../private-route/private-route';
+import { MainPage } from '../../pages/main-page/main-page';
 import { SignInPage } from '../../pages/sign-in-page/sign-in-page';
 import { MyListPage } from '../../pages/my-list-page/my-list-page';
 import { FilmPage } from '../../pages/film-page/film-page';
 import { AddReviewPage } from '../../pages/add-review-page/add-review-page';
 import { PlayerPage } from '../../pages/player-page/player-page';
 import { NotFoundPage } from '../../pages/not-found-page/not-found-page';
-import { PrivateRoute } from '../private-route/private-route';
 
 interface AppProps {
   promoFilmName: string;
