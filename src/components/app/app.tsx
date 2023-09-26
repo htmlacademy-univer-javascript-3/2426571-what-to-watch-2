c7fd3eaa9341e4358f4ebdeb8a473521bdacc918
Yuniya Kim <yuniyakim@gmail.com>
2023-09-27 01:46:06 +0700

    Add routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,17 @@
 import { MainPage } from '../../pages/main-page/main-page';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import { RoutePath, AuthorizationStatus } from '../../types/enums';
+import { SignInPage } from '../../pages/sign-in-page/sign-in-page';
+import { MyListPage } from '../../pages/my-list-page/my-list-page';
+import { FilmPage } from '../../pages/film-page/film-page';
+import { AddReviewPage } from '../../pages/add-review-page/add-review-page';
+import { PlayerPage } from '../../pages/player-page/player-page';
+import { NotFoundPage } from '../../pages/not-found-page/not-found-page';
+import { PrivateRoute } from '../private-route/private-route';
 
 interface AppProps {
   promoFilmName: string;
@@ -7,7 +20,24 @@ interface AppProps {
 }
 
 export const App = (props: AppProps) => (
-  <MainPage
-    {...props}
-  />
+  <BrowserRouter>
+    <Routes>
+      <Route path={RoutePath.Main}>
+        <Route index element={<MainPage {...props} />} />
+        <Route path={RoutePath.SignIn} element={<SignInPage />} />
+        <Route path={RoutePath.MyList} element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <MyListPage />
+          </PrivateRoute>
+          }
+        />
+        <Route path={RoutePath.Films}>
+          <Route path={RoutePath.Film} element={<FilmPage />} />
+          <Route path={RoutePath.AddReview} element={<AddReviewPage />} />
+        </Route>
+        <Route path={RoutePath.Player} element={<PlayerPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
 );
