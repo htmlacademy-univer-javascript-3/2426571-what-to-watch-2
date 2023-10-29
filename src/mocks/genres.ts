bacb7055483fb66edd92691fed2e65bfff9f12bc
Yuniya Kim <yuniyakim@gmail.com>
2023-10-29 19:24:17 +0700

    Update genres mock

--- src/mocks/genres.ts
+++ src/mocks/genres.ts
@@ -1,40 +1,17 @@
+import { ALL_GENRES } from '../types/consts';
 import { IGenre } from '../types/interfaces';
+import { films } from './films';
 
-export const genres: IGenre[] = [
-  {
-    id: 0,
-    name: 'All genres',
-  },
-  {
-    id: 1,
-    name: 'Comedies',
-  },
-  {
-    id: 2,
-    name: 'Crime',
-  },
-  {
-    id: 3,
-    name: 'Documentary',
-  },
-  {
-    id: 4,
-    name: 'Horror',
-  },
-  {
-    id: 5,
-    name: 'Kids & Family',
-  },
-  {
-    id: 6,
-    name: 'Romance',
-  },
-  {
-    id: 7,
-    name: 'Sci-Fi',
-  },
-  {
-    id: 8,
-    name: 'Thrillers',
-  },
-];
+const genresNames: string[] = [];
+films.forEach((film) => {
+  if (film.genres && film.genres !== '') {
+    genresNames.push(...film.genres.split(', '));
+  }
+});
+
+export const genres: IGenre[] = [...new Set([ALL_GENRES, ...genresNames])].map((genre, index) => (
+  <IGenre>{
+    id: index,
+    name: genre,
+  }
+));
