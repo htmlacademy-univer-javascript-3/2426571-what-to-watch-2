c834d9bc222ac55739850ba4081cbfc8237abe82
Yuniya Kim <yuniyakim@gmail.com>
2023-12-11 01:01:07 +0700

    Add mock reviews

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,18 @@
+import { IReview } from '../types/interfaces';
+
+export const reviews: IReview[] = [
+  {
+    id: '1',
+    user:  'Christina',
+    rating: 7,
+    comment: 'A movie that will take you to another world full of emotions.',
+    date: '2022-06-02T12:25:36.946Z'
+  },
+  {
+    id: '2',
+    user: 'Emely',
+    rating: 7.5,
+    comment: 'Poised to be an instant classic, almost everything about this film is phenomenal - the acting, the cinematography, the discography, etc.',
+    date: '2022-06-01T12:25:36.946Z'
+  }
+];
